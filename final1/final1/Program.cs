f31132ab0e22446990dabc958e04dbee829c35e6
Aizhan Sapina <aizhansapina@gmail.com>
2017-05-20 09:26:48 +0600

    26

--- final1/final1/Program.cs
+++ final1/final1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace final1
 {
@@ -10,6 +11,30 @@ namespace final1
     {
         static void Main(string[] args)
         {
+            DirectoryInfo directory = new DirectoryInfo(@"C:\Users\Айжан\Desktop\Final\final1\final1\bin\Debug\Task1");
+            FileSystemInfo[] file = directory.GetFileSystemInfos();
+            foreach (FileSystemInfo f in file)
+            {
+                Console.WriteLine(f.Name + ":");
+            }
+
+            Console.ReadKey();
+
+           /* StreamReader sw = new StreamReader(@"C:\Users\Айжан\Desktop\Final\final1\final1\bin\Debug\Task1\file1.txt");
+            StreamReader sw2 = new StreamReader(@"C:\Users\Айжан\Desktop\Final\final1\final1\bin\Debug\Task1\file2.txt");
+            StreamReader sw3 = new StreamReader(@"C:\Users\Айжан\Desktop\Final\final1\final1\bin\Debug\Task1\file3.txt");
+
+            string[] s = sw.ReadToEnd().Split();
+            
+            for (int i = 1; i <= s.Length; i++)
+            {
+                int a = int.Parse(s[0]);
+                int b = int.Parse(s[1]);
+
+                if(a % 1)
+            }
+            */
+
         }
     }
 }
