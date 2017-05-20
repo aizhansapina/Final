614a58d01b13210f5d96a653985212e5dc0222cd
Aizhan Sapina <aizhansapina@gmail.com>
2017-05-20 11:21:20 +0600

    11:20

--- final4/final4/Form1.cs
+++ final4/final4/Form1.cs
@@ -12,14 +12,40 @@ namespace final4
 {
     public partial class Form1 : Form
     {
+        public static int x = 200;
+        public static int y = 200;
+        //Circle circle = new Circle(x, y);
+        //SolidBrush b = new SolidBrush(Color.Gainsboro);
+        SolidBrush b2 = new SolidBrush(Color.BlueViolet);
         public Form1()
         {
             InitializeComponent();
         }
+       
+      
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+
+        }
+
+
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //e.Graphics.FillPath(b, circle.path);
+            e.Graphics.FillEllipse(b2, x + 30, y + 70, 100, 100);
+            //e.Graphics.FillEllipse(b2, x + 120, y + 70, 50, 50);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            y += 5;
+            //circle = new Circle(x, y);
+            Refresh();
         }
     }
 }
+
+    
