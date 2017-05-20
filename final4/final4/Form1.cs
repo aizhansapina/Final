0ca7d037d968325967c4d6c1b73aafc737d374c6
Aizhan Sapina <aizhansapina@gmail.com>
2017-05-20 12:55:02 +0600

    final exam

--- final4/final4/Form1.cs
+++ final4/final4/Form1.cs
@@ -12,9 +12,11 @@ namespace final4
 {
     public partial class Form1 : Form
     {
+        bool mouseclick = false;
+        int cnt = 0;
         public static int x = 100;
         public static int y = 100;
-        //Circle circle = new Circle(x, y);
+        
         
         SolidBrush b2 = new SolidBrush(Color.BlueViolet);
 
@@ -31,17 +33,29 @@ namespace final4
         
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            //e.Graphics.FillPath(b2, circle.path);
-            e.Graphics.FillEllipse(b2, x + 30, y + 70, 100, 100);
+            
+            e.Graphics.FillEllipse(b2, x + 30, y + 70, 50, 50);
+
+            if (cnt != 0)
+            {
+                e.Graphics.FillEllipse(b2, x + 30, y + 70, 50, 50);
+            }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             y += 3;
-            //circle = new Circle(x, y);
-
+            
             Refresh();
         }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            mouseclick = true;
+            cnt++;
+        }
+
+       
     }
 }
 
