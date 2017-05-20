7847561a4607f135cb85df48e9c6ccc749872b9e
Aizhan Sapina <aizhansapina@gmail.com>
2017-05-20 10:00:26 +0600

    10:00

--- final2/final2/Form1.cs
+++ final2/final2/Form1.cs
@@ -12,9 +12,28 @@ namespace final2
 {
     public partial class Form1 : Form
     {
+        int cnt = 1;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            int a = cnt++;
+            Button button = sender as Button;
+            button.Text = a.ToString();
+            textBox1.Text = a.ToString();
+
+            if (a % 2 == 0)
+            {
+                textBox1.Text = a.ToString();
+            }
         }
     }
 }
